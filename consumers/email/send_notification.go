22d630d3bead1bea2622306234209c2d7771686e
Adam Ananda Santoso <adamananda02@gmail.com>
2024-11-17 12:13:17 +0700

    refactor: update get smtp connection

--- consumers/email/send_notification.go
+++ consumers/email/send_notification.go
@@ -2,24 +2,22 @@ package email
 
 import (
 	"fmt"
+	"notification-service/config"
 	"notification-service/helpers"
 
 	"gopkg.in/gomail.v2"
 )
 
 func SendNotification(messageBody *helpers.MessageBody) error {
-	smtpServer := "smtp.mailtrap.io"
-	smtpPort := 587
-	username := "3d8689177baa73"
-	password := "e8d5b794377601"
+	config := config.GetSMTPConfig()
 
 	mailer := gomail.NewMessage()
-	mailer.SetHeader("From", "adamanandasantoso@gmail.com")
+	mailer.SetHeader("From", config.EmailFrom)
 	mailer.SetHeader("To", messageBody.SendTo)
 	mailer.SetHeader("Subject", "Success")
 	mailer.SetBody("text/plain", messageBody.Message)
 
-	dialer := gomail.NewDialer(smtpServer, smtpPort, username, password)
+	dialer := gomail.NewDialer(config.Server, config.Port, config.Username, config.Password)
 
 	if err := dialer.DialAndSend(mailer); err != nil {
 		return fmt.Errorf("failed to send email: %v", err)
