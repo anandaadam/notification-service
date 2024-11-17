5fe47d26e8e9444a8ddd2ce87dbf2b98682929d5
Adam Ananda Santoso <adamananda02@gmail.com>
2024-11-17 18:28:07 +0700

    refactor: add new field to message body struct consumer

--- helpers/message_body.go
+++ helpers/message_body.go
@@ -3,6 +3,7 @@ package helpers
 type MessageBody struct {
 	EventType string `json:"eventType"`
 	MediaType string `json:"mediaType"`
+	Subject   string `json:"subject"`
 	SendTo    string `json:"sendTo"`
 	Message   string `json:"message"`
 }
